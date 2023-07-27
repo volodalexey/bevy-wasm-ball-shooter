6db365898f7ae7e2defd949f86fb95d49635f0c3
Alexey <volodalexey@gmail.com>
2023-07-27 14:37:29 +0300

    Disabled physics

--- src/gameplay/physics/mod.rs
+++ src/gameplay/physics/mod.rs
@@ -1,9 +1,6 @@
 use bevy::prelude::{App, Plugin, Vec3};
 
-use bevy_rapier3d::{
-    prelude::{NoUserData, RapierConfiguration, RapierPhysicsPlugin},
-    render::RapierDebugRenderPlugin,
-};
+use bevy_rapier3d::prelude::{NoUserData, RapierConfiguration, RapierPhysicsPlugin};
 
 pub struct PhysicsPlugin;
 
@@ -11,7 +8,7 @@ impl Plugin for PhysicsPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins((
             RapierPhysicsPlugin::<NoUserData>::default(),
-            RapierDebugRenderPlugin::default(),
+            // RapierDebugRenderPlugin::default(),
         ))
         .insert_resource(RapierConfiguration {
             gravity: Vec3::ZERO,
